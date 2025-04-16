0bb2bed412a1215cc56873b6fbf51c57284dbec5
sonkiee <annagu.kennedy@gmail.com>
2025-04-16 11:57:37 +0000

    Apr 16, 2025, 12:57 PM

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -3,7 +3,7 @@ import { ShoppingCart } from "lucide-react";
 export function CartIcon() {
   return (
     <div className="fixed flex b-0 right-0">
-      <ShoppingCart />;
+      <ShoppingCart />
     </div>
   );
 }
