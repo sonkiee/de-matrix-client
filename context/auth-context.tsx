240450a687779521e743a0af97590fed10d9e5b7
sonkiee <annagu.kennedy@gmail.com>
2025-04-18 11:40:02 +0000

    Apr 18, 2025, 12:40 PM

--- context/auth-context.tsx
+++ context/auth-context.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { api } from "@/services/axiosInstance";
 import { AuthContextType, User } from "@/types";
 import React, { createContext, useContext, useState } from "react";
 
@@ -21,15 +22,23 @@ export const AuthProvider: React.FC<AuthContextProviderProps> = ({
     address: "123 Main St, Anytown, USA",
   });
 
-  const signIn = (email: string, password: string) => {
-    // Simulate authentication
-    setUser({
-      name: "John Doe",
-      email,
-      password,
-      role: "user",
-      address: "123 Main St, Anytown, USA",
-    });
+  const signIn = async (email: string, password: string) => {
+    try {
+      const response = await api.post("/auth/signin", { email, password });
+      console.log("Successfully signed in:", response.data);
+
+      // Simulate authentication
+      // setUser({
+      //   name: "John Doe",
+      //   email,
+      //   password,
+      //   role: "user",
+      //   address: "123 Main St, Anytown, USA",
+      // });
+    } catch (error) {
+      // Handle authentication error
+      console.log("Failed to sign in:", error);
+    }
   };
 
   return (
